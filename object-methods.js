cbef74d93570697b144b1db075765225ca5f7025
adakamahesh <adakamahesh@gmail.com>
2024-03-23 21:48:05 +0530

    object-methods

--- object-methods.js
+++ object-methods.js
@@ -15,7 +15,7 @@ const me = Object.create(person);
 me.name='mahesh';
 me.isHuman=true;
 me.printIntroduction();
-//Object.entries()
+//Object.entries()->this is used to display the all enteries of objecs
 const object1 = {
   a: 'somestring',
   b: 42,
@@ -29,10 +29,35 @@ console.log(Object.is('1', 1));
 console.log(Object.is(NaN, NaN));
 const obj = {};
 console.log(Object.is(obj, {}));
-//Object.values()->this is used to displat the all values of object
+//Object.values()->this is used to display the all values in array forme of object 
 const object = {
   a: 'somestring',
   b: 42,
   c: false,
 };
 console.log(Object.values(object));
+//Object.defineProperties()->this is used to defines new or modifies directly on object
+const object2 = {};
+Object.defineProperties(object2, {
+  property1: {
+    value: 42,
+    writable: true,
+  },
+  property2: {},
+});
+console.log(object2.property1);
+//Object.getOwnPropertyNames()->it returns of all properties found directly in a given object
+//Object.keys()
+const object3 = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+console.log(Object.getOwnPropertyNames(object3));
+//Object.hasOwn()->it is used to returns true if specified object has the indicated property aas its own property otherwise return false
+//Object.prototype.hasOwnProperty()
+const object4= {
+  prop: 'exists',
+};
+console.log(Object.hasOwn(object4, 'prop'));
+console.log(Object.hasOwn(object4, 'toString'));
